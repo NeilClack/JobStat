515454398c145656f6bf3130f326fac635d8c8b2
NeilClack <c.neilclack@gmail.com>
2022-11-11 15:32:44 -0600

    Query jobs

--- job_stats/jobs.py
+++ job_stats/jobs.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, g, session, url_for, request, render_template
+from flask import Blueprint, g, session, url_for, request, render_template, jsonify
+from .models import Listing, ListingSchema
 
 bp = Blueprint('jobs', __name__, url_prefix="/")
 
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@bp.route('/get', methods=["GET"])
+def get_all():
+    title = request.args.get('title', default=None, type=str)
+
+    # Filter by keyword in title
+    if title is not None:
+        title = f"%{title}%"
+        listings = Listing.query.filter(Listing.title.like(title))
+    else:
+        listings = Listing.query.all()
+    
+    
+    listing_schema = ListingSchema(many=True)
+
+    return jsonify(listing_schema.dump(listings)), 200
