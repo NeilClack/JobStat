b412af4fd4d81113c835b0c766546eac21f0b834
NeilClack <employ.neil@gmail.com>
2022-12-16 13:34:12 -0600

    Correct status codes and add failure tests.

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_create_add_one(client):
     response = client.post(
         "/api/add",
@@ -16,7 +13,7 @@ def test_create_add_one(client):
         },
     )
 
-    assert response.status_code == 200
+    assert response.status_code == 201
 
 
 def test_create_add_many(client):
@@ -46,7 +43,7 @@ def test_create_add_many(client):
         ],
     )
 
-    assert response.status_code == 200
+    assert response.status_code == 201
 
 
 def test_create_add_fail_one(client):
@@ -54,8 +51,35 @@ def test_create_add_fail_one(client):
         "/api/add",
         json={
             "title": 25,
+            "company": 200,
+            "location": {"string": "text"},
+            "salary": "Notanumber",
+            "summary": "This is not a real job listing. Delete this later. DELETE",
+            "url": "http://Dontforgettochangethiseachtime",
+            "posted": "2022-12-13T00:05:10.442112",
+            "scraped": "2022-12-13",
+        },
+    )
+
+    ve = response.json["ValidationError"]
+
+    assert response.status_code == 400
+    assert "ValidationError" in response.json
+    assert "title" in ve
+    assert "company" in ve
+    assert "location" in ve
+    assert "salary" in ve
+    assert "url" in ve
+    assert "posted" in ve
+    assert "scraped" in ve
+
+
+def test_integrity_errors(client):
+    response = client.post(
+        "/api/add",
+        json={
             "company": "Not a real company",
-            "location": "anywhere",
+            "location": "Anywhere",
             "salary": 400000,
             "summary": "This is not a real job listing. Delete this later. DELETE",
             "url": "http://Dontforgettochangethiseachtime.com",
@@ -64,4 +88,6 @@ def test_create_add_fail_one(client):
         },
     )
 
-    assert response.status_code == 200
+    # This is a 400 because this can only happen if the request was missing fields.
+    assert response.status_code == 400
+    assert "IntegrityError" in response.json
